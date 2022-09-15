7c606e3a521156281cfa6ed9e824ed931a2a914d
M. Fadhil Mahendra <mfmahendr@gmail.com>
2022-09-15 21:48:14 +0700

    Initialize Model

---
+++ Hatiku/Models/Repository/IAdminPolicyRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hatiku.Models.Repository
+{
+    public interface IAdminPolicyRepository
+    {
+        IEnumerable<AdminPolicy> FetchAll();
+        bool Check(int userId, int policyId);
+        bool Add(AdminPolicy admin);
+        bool Delete(int userId, int policyId);
+    }
+}
