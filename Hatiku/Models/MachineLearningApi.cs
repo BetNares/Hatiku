dd5a0d87fea78a3df6a91dbfc7e196022c6bf9f4
M. Fadhil Mahendra <mfmahendr@gmail.com>
2022-09-16 11:14:43 +0700

    Add MachineLearningApi model

---
+++ Hatiku/Models/MachineLearningApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hatiku.Models
+{
+    public class MlApi
+    {
+        private string id;
+        private string endpoint;
+        private string apiKey;
+        private bool isConnect;
+
+        public string Id { get => id; set => id = value; }
+        public string Endpoint { get => endpoint; set => endpoint = value; }
+        public string ApiKey { get => apiKey; set => apiKey = value; }
+        public bool IsConnect { get => isConnect; set => isConnect = value; }
+
+    }
+}
