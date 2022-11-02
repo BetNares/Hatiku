98dee5d8415a40470b9674c343b9294d9279b7b2
M. Fadhil Mahendra <mfmahendr@gmail.com>
2022-11-02 22:22:23 +0700

    Change and Add Method at Admin Repo

--- Hatiku/Models/Repository/IAdminRepository.cs
+++ Hatiku/Models/Repository/IAdminRepository.cs
@@ -9,7 +9,8 @@ namespace Hatiku.Models.Repository
     public interface IAdminRepository
     {
         IEnumerable<Admin> FetchAll();
-        IEnumerable<Admin> Find(int id);
+        IEnumerable<Admin> FindById(int id);
+        IEnumerable<Admin> FindByValue(string value);
         bool Add(Admin admin);
         bool Edit(Admin admin);
         bool Delete(int id);
