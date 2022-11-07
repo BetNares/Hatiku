22c000805434766f8810afd16a1e3e9914c56d5d
M. Fadhil Mahendra <mfmahendr@gmail.com>
2022-11-07 07:58:34 +0700

    Add Model Validation

---
+++ Hatiku/Presenters/ModelValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hatiku.Presenters
+{
+    public static class ModelValidation
+    {
+        public static void Validate(object objectModel)
+        {
+            string errorMessage = "";
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            ValidationContext validationContext = new ValidationContext(objectModel);
+            
+            if(Validator.TryValidateObject(objectModel, validationContext, validationResults, true))
+            {
+                foreach(var item in validationResults)
+                    errorMessage += $"- {item.ErrorMessage}\n";
+                throw new Exception(errorMessage);
+            }
+        }
+    }
+}
