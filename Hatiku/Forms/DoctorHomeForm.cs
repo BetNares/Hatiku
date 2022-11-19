003ed9836b432363090d1c95f1a91cb5419b58eb
BetNares <btari.f.r@mail.ugm.ac.id>
2022-11-19 20:07:32 +0700

    added some interfaces and presenters
    added: MainEntryPresenter, IMainEntryView
    
    Edited: DoctorHomeForm, MainEntryForm

--- Hatiku/Forms/DoctorHomeForm.cs
+++ Hatiku/Forms/DoctorHomeForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hatiku.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,13 +12,25 @@ using System.Windows.Forms;
 namespace Hatiku.Forms
 {
 
-    public partial class DoctorHomeForm : Form
+    public partial class DoctorHomeForm : Form, IDoctorHomeView
     {
         AboutForm _profileForm;
         InputDataForm _InputDataForm;
+
+        public event EventHandler ShowInputForm;
+        public event EventHandler ShowAboutForm;
+
+
         public DoctorHomeForm()
         {
             InitializeComponent();
+            AssociateAndRaiseEvents();
+        }
+
+        private void AssociateAndRaiseEvents()
+        {
+            this.btnStartTest.Click += delegate { ShowInputForm?.Invoke(this, EventArgs.Empty); };
+            this.btnAbout.Click += delegate { ShowAboutForm?.Invoke(this, EventArgs.Empty); };
         }
 
         private void lblClose_Click(object sender, EventArgs e)
@@ -27,24 +40,24 @@ namespace Hatiku.Forms
 
         private void btnCheckProfile_Click(object sender, EventArgs e)
         {
-            _profileForm = new AboutForm();
+            //_profileForm = new AboutForm();
 
-            _profileForm.Location = this.Location;
-            _profileForm.StartPosition = FormStartPosition.Manual;
-            _profileForm.FormClosing += delegate { this.Show(); };
-            _profileForm.Show();
-            this.Hide();
+            //_profileForm.Location = this.Location;
+            //_profileForm.StartPosition = FormStartPosition.Manual;
+            //_profileForm.FormClosing += delegate { this.Show(); };
+            //_profileForm.Show();
+            //this.Hide();
         }
 
         private void btnStartTest_Click(object sender, EventArgs e)
         {
-            _InputDataForm = new InputDataForm();
+            //_InputDataForm = new InputDataForm();
 
-            _InputDataForm.Location = this.Location;
-            _InputDataForm.StartPosition = FormStartPosition.Manual;
-            _InputDataForm.FormClosing += delegate { this.Show(); };
-            _InputDataForm.Show();
-            this.Hide();
+            //_InputDataForm.Location = this.Location;
+            //_InputDataForm.StartPosition = FormStartPosition.Manual;
+            //_InputDataForm.FormClosing += delegate { this.Show(); };
+            //_InputDataForm.Show();
+            //this.Hide();
         }
     }
 }
---
+++ Hatiku/Presenters/MainEntryPresenter.cs
@@ -0,0 +1,38 @@
+﻿using Hatiku.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hatiku.Presenters
+{
+    public class MainEntryPresenter
+    {
+        private IMainEntryView view;
+        private string connString;
+
+        public MainEntryPresenter(IMainEntryView view, string connString)
+        {
+            this.view = view;
+            this.connString = connString;
+
+            this.view.ShowDoctorHome += ShowDoctorHome;
+            this.view.ShowAdminLogin += ShowAdminLogin;
+
+            ((Form)this.view).Show();
+
+        }
+
+        private void ShowAdminLogin(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowDoctorHome(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
